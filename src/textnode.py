140412334e6853fd1e2aa762f531ade4bf0d8c28
gsk-007 <gsingh16742@gmail.com>
2026-04-10 04:48:30 +0000

    feat: add project files and textnode, ref: remove public dir files

---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN_TEXT = "text"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK_TEXT = "link"
+    IMAGE_TEXT = "image"
+
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text != other.text or self.text_type != other.text_type or self.url != other.url:
+            return False
+
+        return True
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
