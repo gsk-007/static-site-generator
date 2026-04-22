db7b9d70bdfa4e0911602148b629a7c19989741d
gsk-007 <gsingh16742@gmail.com>
2026-04-22 01:30:44 +0000

    refactor: path string to variables

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -17,7 +19,8 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    generate_pages_recursive('./content','./template.html',"./public")
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 main()
